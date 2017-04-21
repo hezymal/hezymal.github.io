c6454a46c1f48970e9d987445253d1d4abd0875d
Zakhar Yakovlev <hezymal@gmail.com>
2017-04-21 10:21:46 +0900

    Their latest version

--- public/javascripts/controllers/form_add_event.js
+++ public/javascripts/controllers/form_add_event.js
@@ -1,9 +1,6 @@
 exports = module.exports = ['$scope', '$http', '$httpParamSerializerJQLike', function ($scope, $http, $httpParamSerializerJQLike) {
   $scope.formData2 = {};
   $scope.addEventForm = function() {
-    console.log($httpParamSerializerJQLike($scope.formData2));
-    console.log($scope.formData2);
-    console.log($.param($scope.formData2));
     $http({
       url     : 'addevent.php',
       method  : 'POST',
--- public/javascripts/controllers/form_feedback.js
+++ public/javascripts/controllers/form_feedback.js
@@ -1,9 +1,6 @@
 exports = module.exports = ['$scope', '$http', '$httpParamSerializerJQLike', function ($scope, $http, $httpParamSerializerJQLike) {
   $scope.formData = {};
   $scope.feedbackForm = function() {
-    console.log($httpParamSerializerJQLike($scope.formData));
-    console.log($scope.formData);
-    console.log($.param($scope.formData));
     $http({
       url     : 'feedback.php',
       method  : 'POST',
